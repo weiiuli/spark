af313c8ae28af3e992fe383558f49fa0bee7c7ae
魏秀利 <weixiuli@ZBMAC-C02W317PH.local>
2018-11-13 11:30:42 +0800

    update ExecutorShuffleInfo

--- core/src/main/scala/org/apache/spark/network/shuffle/protocol/ExecutorShuffleInfo.java
+++ core/src/main/scala/org/apache/spark/network/shuffle/protocol/ExecutorShuffleInfo.java
@@ -34,20 +34,40 @@ public class ExecutorShuffleInfo implements Encodable {
   public final int subDirsPerLocalDir;
   /** Shuffle manager (SortShuffleManager) that the executor is using. */
   public final String shuffleManager;
+  /** Shuffle manager (shuffleCompressEnabled) that the executor and ESS is using. */
+  public final boolean shuffleCompressEnabled;
+  /** Shuffle manager (compressionCodec) that the executor and ESS is using. */
+  public final String compressionCodec ;
+  /** Shuffle manager (serializer) that the executor and ESS is using. */
+  public final String serializer ;
+  /** Shuffle manager (shuffleUnsafeFastMergeEnabled) that the executor and ESS is using. */
+  public final boolean shuffleUnsafeFastMergeEnabled;
+  /** Shuffle manager (fileTransferTo) that the ESS is using. */
+  public final boolean fileTransferTo;
 
   @JsonCreator
   public ExecutorShuffleInfo(
       @JsonProperty("localDirs") String[] localDirs,
       @JsonProperty("subDirsPerLocalDir") int subDirsPerLocalDir,
-      @JsonProperty("shuffleManager") String shuffleManager) {
+      @JsonProperty("shuffleManager") String shuffleManager,
+      @JsonProperty("shuffleCompressEnabled") boolean shuffleCompressEnabled,
+      @JsonProperty("compressionCodec") String compressionCodec,
+      @JsonProperty("serializer") String serializer,
+      @JsonProperty("shuffleUnsafeFastMergeEnabled") boolean shuffleUnsafeFastMergeEnabled,
+      @JsonProperty("fileTransferTo") boolean fileTransferTo) {
     this.localDirs = localDirs;
     this.subDirsPerLocalDir = subDirsPerLocalDir;
     this.shuffleManager = shuffleManager;
+    this.shuffleCompressEnabled = shuffleCompressEnabled;
+    this.compressionCodec = compressionCodec;
+    this.serializer = serializer;
+    this.shuffleUnsafeFastMergeEnabled = shuffleUnsafeFastMergeEnabled;
+    this.fileTransferTo = fileTransferTo;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(subDirsPerLocalDir, shuffleManager) * 41 + Arrays.hashCode(localDirs);
+    return Objects.hashCode(subDirsPerLocalDir, shuffleManager,compressionCodec,serializer) * 41 +Objects.hashCode(shuffleCompressEnabled,shuffleUnsafeFastMergeEnabled,fileTransferTo) + Arrays.hashCode(localDirs);
   }
 
   @Override
@@ -56,6 +76,11 @@ public class ExecutorShuffleInfo implements Encodable {
       .add("localDirs", Arrays.toString(localDirs))
       .add("subDirsPerLocalDir", subDirsPerLocalDir)
       .add("shuffleManager", shuffleManager)
+      .add("shuffleCompressEnabled", shuffleCompressEnabled)
+      .add("compressionCodec", compressionCodec)
+      .add("serializer", serializer)
+      .add("shuffleUnsafeFastMergeEnabled", shuffleUnsafeFastMergeEnabled)
+      .add("fileTransferTo", fileTransferTo)
       .toString();
   }
 
@@ -65,7 +90,12 @@ public class ExecutorShuffleInfo implements Encodable {
       ExecutorShuffleInfo o = (ExecutorShuffleInfo) other;
       return Arrays.equals(localDirs, o.localDirs)
         && Objects.equal(subDirsPerLocalDir, o.subDirsPerLocalDir)
-        && Objects.equal(shuffleManager, o.shuffleManager);
+        && Objects.equal(shuffleManager, o.shuffleManager)
+        && Objects.equal(shuffleCompressEnabled, o.shuffleCompressEnabled)
+        && Objects.equal(compressionCodec, o.compressionCodec)
+        && Objects.equal(serializer, o.serializer)
+        && Objects.equal(shuffleUnsafeFastMergeEnabled, o.shuffleUnsafeFastMergeEnabled)
+        && Objects.equal(fileTransferTo, o.fileTransferTo);
     }
     return false;
   }
@@ -74,7 +104,12 @@ public class ExecutorShuffleInfo implements Encodable {
   public int encodedLength() {
     return Encoders.StringArrays.encodedLength(localDirs)
         + 4 // int
-        + Encoders.Strings.encodedLength(shuffleManager);
+        + Encoders.Strings.encodedLength(shuffleManager)
+        + 1
+        + Encoders.Strings.encodedLength(compressionCodec)
+        + Encoders.Strings.encodedLength(serializer)
+        + 1
+        + 1;
   }
 
   @Override
@@ -82,12 +117,22 @@ public class ExecutorShuffleInfo implements Encodable {
     Encoders.StringArrays.encode(buf, localDirs);
     buf.writeInt(subDirsPerLocalDir);
     Encoders.Strings.encode(buf, shuffleManager);
+    buf.writeBoolean(shuffleCompressEnabled);
+    Encoders.Strings.encode(buf, compressionCodec);
+    Encoders.Strings.encode(buf, serializer);
+    buf.writeBoolean(shuffleUnsafeFastMergeEnabled);
+    buf.writeBoolean(fileTransferTo);
   }
 
   public static ExecutorShuffleInfo decode(ByteBuf buf) {
     String[] localDirs = Encoders.StringArrays.decode(buf);
     int subDirsPerLocalDir = buf.readInt();
     String shuffleManager = Encoders.Strings.decode(buf);
-    return new ExecutorShuffleInfo(localDirs, subDirsPerLocalDir, shuffleManager);
+    boolean shuffleCompressEnabled = buf.readBoolean();
+    String compressionCodec = Encoders.Strings.decode(buf);
+    String serializer = Encoders.Strings.decode(buf);
+    boolean shuffleUnsafeFastMergeEnabled = buf.readBoolean();
+    boolean fileTransferTo = buf.readBoolean();
+    return new ExecutorShuffleInfo(localDirs, subDirsPerLocalDir, shuffleManager,shuffleCompressEnabled,compressionCodec,serializer,shuffleUnsafeFastMergeEnabled,fileTransferTo);
   }
 }
